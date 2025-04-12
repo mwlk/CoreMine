4a49dbb34f67349370371001cf0c5a7d7cb2ddf3
mwlk <mirkoivowlk@gmail.com>
2025-04-12 10:52:06 -0300

    update dependencies

--- MiVivero.Repository/DependencyInjection.cs
+++ MiVivero.Repository/DependencyInjection.cs
@@ -1,16 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using MiVivero.ApplicationBusiness.Interfaces;
-using MiVivero.Entities;
-
-namespace MiVivero.Repository
+﻿namespace MiVivero.Repository
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddRepositories(this IServiceCollection services)
-        {
-            services.AddTransient<IRepository<Product>, ProductsRepository>();
-
-            return services;
-        }
+        
     }
 }

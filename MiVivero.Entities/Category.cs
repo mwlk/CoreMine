3765e640dd0c7d92b1bed3ce82a04b6030eb924b
mwlk <mirkoivowlk@gmail.com>
2025-04-12 10:46:05 -0300

    update category class

--- MiVivero.Entities/Category.cs
+++ MiVivero.Entities/Category.cs
@@ -4,5 +4,9 @@ namespace MiVivero.Entities
 {
     public class Category : NameableEntity
     {
+        public string Code { get; set; }
+        public int? ParentId { get; set; }
+        public Category? Parent { get; set; }
+        public ICollection<Category> ChildCategories { get; set; } = new List<Category>();
     }
 }
--- MiVivero.Entities/Product.cs
+++ MiVivero.Entities/Product.cs
@@ -4,5 +4,7 @@ namespace MiVivero.Entities
 {
     public class Product : NameableEntity
     {
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }

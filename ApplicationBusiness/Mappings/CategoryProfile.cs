555ea134025a0b3315308cc7c4d2c9e90b70a3c8
mwlk <mirkoivowlk@gmail.com>
2025-04-12 10:47:56 -0300

    move mapper profiles

---
+++ ApplicationBusiness/Mappings/CategoryProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MiVivero.ApplicationBusiness.DTOs;
+using MiVivero.ApplicationBusiness.ViewModels;
+using MiVivero.Entities;
+
+namespace MiVivero.ApplicationBusiness.Mappings
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<Category, CategoryViewModel>();
+
+            CreateMap<CategoryPostDto, Category>();
+        }
+    }
+}
---
+++ ApplicationBusiness/Mappings/ProductProfile.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using MiVivero.Entities;
+using MiVivero.ApplicationBusiness.DTOs;
+using MiVivero.ApplicationBusiness.ViewModels;
+
+namespace MiVivero.ApplicationBusiness.Mappings
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductViewModel>()
+                .ForMember(dest => dest.Categories, orig => orig.MapFrom(ent => GetCategoryHierarchy(ent.Category)));
+
+            CreateMap<ProductPostDto, Product>();
+        }
+
+        private static List<CategoryViewModel> GetCategoryHierarchy(Category? category)
+        {
+            var categories = new List<CategoryViewModel>();
+
+            while (category != null)
+            {
+                categories.Insert(0, new CategoryViewModel
+                {
+                    Id = category.Id,
+                    Code = category.Code,
+                    Name = category.Name
+                });
+
+                category = category.Parent; // Subimos en la jerarquía hasta llegar al nodo raíz
+            }
+
+            return categories;
+        }
+    }
+
+
+}

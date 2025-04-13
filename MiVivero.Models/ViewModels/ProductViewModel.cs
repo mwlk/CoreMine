7dd00858f2965fe44272e839e29edd0df97ed2a7
mwlk <mirkoivowlk@gmail.com>
2025-04-13 18:26:18 -0300

    add code into product view models

--- MiVivero.Models/ViewModels/ProductViewModel.cs
+++ MiVivero.Models/ViewModels/ProductViewModel.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Code { get; set; }
         public List<CategoryViewModel> Categories { get; set; } = new List<CategoryViewModel>();
     }
 }

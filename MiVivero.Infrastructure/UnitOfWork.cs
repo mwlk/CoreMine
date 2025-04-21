27d92fb0503b3eaf8e5ac27a0523036e3fe9b9af
mwlk <mirkoivowlk@gmail.com>
2025-04-20 22:00:55 -0300

    add implementation for UOW

---
+++ MiVivero.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using MiVivero.ApplicationBusiness.Interfaces;
+using MiVivero.Data;
+
+namespace MiVivero.Infraestructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        private IDbContextTransaction? _currentTransaction;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task BeginTransactionAsync()
+        {
+            if (_currentTransaction != null) return;
+
+            _currentTransaction = await _context.Database.BeginTransactionAsync();
+        }
+
+        public async Task CommitTransactionAsync()
+        {
+            if (_currentTransaction == null) return;
+
+            await _context.SaveChangesAsync();
+            await _currentTransaction.CommitAsync();
+            await _currentTransaction.DisposeAsync();
+            _currentTransaction = null;
+        }
+
+        public async Task RollbackTransactionAsync()
+        {
+            if (_currentTransaction == null) return;
+
+            await _currentTransaction.RollbackAsync();
+            await _currentTransaction.DisposeAsync();
+            _currentTransaction = null;
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}

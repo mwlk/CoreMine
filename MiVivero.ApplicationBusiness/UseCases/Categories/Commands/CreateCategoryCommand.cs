98c1f75b5247002984f56faeab8ebf2b800a1372
mwlk <mirkoivowlk@gmail.com>
2025-04-20 21:59:41 -0300

    rename command

---
+++ MiVivero.ApplicationBusiness/UseCases/Categories/Commands/CreateCategoryCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace MiVivero.ApplicationBusiness.UseCases.Categories.Commands
+{
+    public class CreateCategoryCommand : IRequest<int>
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public int? ParentId { get; set; }
+    }
+}

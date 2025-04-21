db3f20ac590a8d970f91097c272c3326ce907826
mwlk <mirkoivowlk@gmail.com>
2025-04-21 02:02:16 -0300

    update categories levess

---
+++ MiVivero.Client/Models/UI/SelectableCategoryLevel.cs
@@ -0,0 +1,10 @@
+﻿using MiVivero.Models.ViewModels;
+
+namespace MiVivero.Client.Models.UI
+{
+    public class SelectableCategoryLevel
+    {
+        public List<CategoryViewModel> Options { get; set; } = new();
+        public int? SelectedCategoryId { get; set; }
+    }
+}

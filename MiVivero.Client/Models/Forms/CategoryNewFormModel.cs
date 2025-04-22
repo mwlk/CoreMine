743d99c4dabb4a8e7927e382b83ee5a41f3eb024
mwlk <mirkoivowlk@gmail.com>
2025-04-22 02:56:06 -0300

    add form validation to code & name

---
+++ MiVivero.Client/Models/Forms/CategoryNewFormModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiVivero.Client.Models.Forms
+{
+    public class CategoryNewFormModel
+    {
+        [Required(ErrorMessage = "El código es obligatorio")]
+        public string Code { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Name { get; set; }
+    }
+}

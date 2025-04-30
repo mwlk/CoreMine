d3cf9a98401b6400dc145a57b189c867609acd30
mwlk <mirkoivowlk@gmail.com>
2025-04-30 02:35:19 -0300

    add implementation for radzen

--- CoreMine.Client/Program.cs
+++ CoreMine.Client/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using CoreMine.Client;
-using CoreMine.Client.Services;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-builder.Services.AddSingleton<ThemeService>();
+builder.Services.AddSingleton<CoreMine.Client.Services.ThemeService>();
+
+builder.Services.AddRadzenComponents().AddRadzenComponents();
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");

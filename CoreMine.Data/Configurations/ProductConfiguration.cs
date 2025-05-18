a05f231f99b0a703b79e9957287e9d1d759da989
mwlk <mirkoivowlk@gmail.com>
2025-05-18 04:07:58 -0300

    add entity's configuration files

--- CoreMine.Data/Configurations/ProductConfiguration.cs
+++ CoreMine.Data/Configurations/ProductConfiguration.cs
@@ -11,7 +11,21 @@ namespace CoreMine.Data.Configurations
             builder.ToTable("Products");
 
             builder.HasKey(p => p.Id);
-            builder.Property(p => p.Name).IsRequired().HasMaxLength(200);
+
+            builder.HasIndex(p => p.Id);
+
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(p => p.Description)
+                .HasMaxLength(200);
+
+            builder.Property(p => p.UnitPrice)
+                .HasPrecision(18, 4);
+
+            builder.Property(p => p.CreatedAt)
+                .IsRequired();
         }
     }
 }
---
+++ CoreMine.Data/Configurations/SupplierConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using CoreMine.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CoreMine.Data.Configurations
+{
+    public class SupplierConfiguration : IEntityTypeConfiguration<Supplier>
+    {
+        public void Configure(EntityTypeBuilder<Supplier> builder)
+        {
+            builder.ToTable("Suppliers");
+
+            builder.HasKey(p => p.Id);
+
+            builder.HasIndex(p => p.Id);
+
+            builder.Property(p => p.Name)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(p => p.Surname)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(p => p.Contact)
+                .HasMaxLength(100);
+
+            builder.Property(p => p.Phone)
+                .HasMaxLength(20);
+
+            builder.Property(p => p.CreatedAt)
+                .IsRequired();
+        }
+    }
+}

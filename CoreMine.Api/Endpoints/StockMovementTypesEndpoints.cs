c177e4117d9fcd63e13777a5499f8102fee4795b
mwlk <mirkoivowlk@gmail.com>
2025-05-18 23:38:16 -0300

    feat: add get for stock movement types

---
+++ CoreMine.Api/Endpoints/StockMovementTypesEndpoints.cs
@@ -0,0 +1,27 @@
+﻿using CoreMine.ApplicationBusiness.Interfaces.Shared;
+using CoreMine.ApplicationBusiness.UseCases.StockMovementTypes.Queries;
+using CoreMine.Models.Common;
+using CoreMine.Models.ViewModels;
+
+namespace CoreMine.Api.Endpoints
+{
+    public static partial class EndpointRegister
+    {
+        public static void MapStockMovementTypesEndpoints(this IEndpointRouteBuilder app)
+        {
+            var group = app.MapGroup("/api/stockMovementTypes")
+                .WithTags("StockMovementTypes");
+
+            // GET: api/stockMovementTypes
+            group.MapGet("/", async (
+                [AsParameters] GetStockMovementTypesQuery query,
+                IQueryHandler<GetStockMovementTypesQuery, PagedResult<StockMovementTypeViewModel>> handler,
+                CancellationToken cancellationToken) =>
+            {
+                var result = await handler.HandleAsync(query, cancellationToken);
+                return Results.Ok(result);
+            })
+                .Produces<PagedResult<StockMovementTypeViewModel>>(StatusCodes.Status200OK);
+        }
+    }
+}

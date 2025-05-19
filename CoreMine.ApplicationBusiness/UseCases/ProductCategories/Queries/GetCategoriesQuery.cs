3d42d7c3def249c4e8a65e6ea7da269d4f4adcaf
mwlk <mirkoivowlk@gmail.com>
2025-05-18 21:52:23 -0300

    feat: add unit of measures get

--- CoreMine.ApplicationBusiness/UseCases/ProductCategories/Queries/GetCategoriesQuery.cs
+++ CoreMine.ApplicationBusiness/UseCases/ProductCategories/Queries/GetCategoriesQuery.cs
@@ -1,12 +1,13 @@
-﻿namespace CoreMine.ApplicationBusiness.UseCases.Categories.Queries
+﻿using CoreMine.ApplicationBusiness.Common.Queries;
+
+namespace CoreMine.ApplicationBusiness.UseCases.Categories.Queries
 {
-    public class GetCategoriesQuery 
+    public class GetCategoriesQuery : PaginatedQuery
     {
         public int? Id { get; set; }
         public string? Name { get; set; }
         public int? ParentId { get; set; }
         public bool? IsParent { get; set; } = false;
-        public int? PageSize { get; set; }
-        public int? PageNumber { get; set; }
+
     }
 }

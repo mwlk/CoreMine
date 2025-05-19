d160cbbc9d1a68a68f000e82e0d140e6d0cf47cd
mwlk <mirkoivowlk@gmail.com>
2025-05-19 02:10:14 -0300

    feat: add middleware for exeption

---
+++ CoreMine.Api/Middlewares/Extensions/ExceptionMiddlewareExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace CoreMine.Api.Middlewares.Extensions
+{
+    public static class ExceptionMiddlewareExtension
+    {
+        public static IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}

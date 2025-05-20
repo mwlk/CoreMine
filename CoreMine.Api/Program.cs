9575fc412a1ffcdc03642f2a4f47808621354d66
mwlk <mirkoivowlk@gmail.com>
2025-05-20 18:47:25 -0300

    refactor program

--- CoreMine.Api/Program.cs
+++ CoreMine.Api/Program.cs
@@ -9,73 +9,74 @@ using CoreMine.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
-internal class Program
+namespace CoreMine.Api
 {
-    private static async Task Main(string[] args)
+    internal static class Program
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
-
-        #region CONFIGURATIONS
-
-        // Configurar Connection String
-        builder.Services.AddPersistence(builder.Configuration);
-
-        // Configurar MediatR y AutoMapper
-        builder.Services.AddApplicationBusiness();
-
-        // Configurar Repositories
-        builder.Services.AddInfraestructure();
-
-        builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-        #endregion
-
-        #region CORS
-        builder.Services.AddCors(options =>
+        private static async Task Main(string[] args)
         {
-            options.AddPolicy("AllowBlazorClientOrigin",
-                builder => builder.WithOrigins("https://localhost:7008")
-                                    .AllowAnyHeader()
-                                    .AllowAnyMethod());
-        });
-        #endregion
+            var builder = WebApplication.CreateBuilder(args);
 
-        builder.Services.AddControllers(); 
 
-        var app = builder.Build();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.MapOpenApi();
-            app.MapScalarApiReference();
+            #region CONFIGURATIONS
+
+            // Configurar Connection String
+            builder.Services.AddPersistence(builder.Configuration);
+
+            // Configurar MediatR y AutoMapper
+            builder.Services.AddApplicationBusiness();
+
+            // Configurar Repositories
+            builder.Services.AddInfraestructure();
+
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            #endregion
+
+            #region CORS
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowBlazorClientOrigin",
+                    builder => builder.WithOrigins("https://localhost:7008")
+                                        .AllowAnyHeader()
+                                        .AllowAnyMethod());
+            });
+            #endregion
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                app.MapScalarApiReference();
+            }
+
+            app.UseExceptionMiddleware();
+
+            app.UseHttpsRedirection();
+
+            app.RegisterEndpoints();
+
+            app.UseCors("AllowBlazorClientOrigin");
+
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+                await context.Database.MigrateAsync();
+
+                await LoadDatabase.InsertData(context);
+            }
+
+            app.MapControllers();
+
+            await app.RunAsync();
         }
-
-        app.UseExceptionMiddleware();
-
-        app.UseHttpsRedirection();
-
-        app.RegisterEndpoints();
-
-        app.UseCors("AllowBlazorClientOrigin");
-
-        
-        using (var scope = app.Services.CreateScope())
-        {
-            var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-            
-            await context.Database.MigrateAsync();
-
-            await LoadDatabase.InsertData(context);
-        }
-
-        app.MapControllers(); 
-
-        await app.RunAsync();
     }
 }
-
-

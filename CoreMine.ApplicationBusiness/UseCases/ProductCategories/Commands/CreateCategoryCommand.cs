8b9806ede2d16f832a79b49bf0d131aff75ae905
mwlk <mirkoivowlk@gmail.com>
2025-05-21 01:56:26 -0300

    refactor: add try and separate save change

--- CoreMine.ApplicationBusiness/UseCases/ProductCategories/Commands/CreateCategoryCommand.cs
+++ CoreMine.ApplicationBusiness/UseCases/ProductCategories/Commands/CreateCategoryCommand.cs
@@ -2,8 +2,21 @@
 {
     public class CreateCategoryCommand 
     {
-        public string Name { get; set; }
-        public string Code { get; set; }
+        public string Name { get; set; } = default!;
+        public string Code { get; set; } = default!;
         public int? ParentId { get; set; }
+
+        public void Validate()
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                throw new ArgumentException("El nombre es obligatorio");
+            }
+
+            if (string.IsNullOrEmpty(Code))
+            {
+                throw new ArgumentException("El código es obligatorio");
+            }
+        }
     }
 }

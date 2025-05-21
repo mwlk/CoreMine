839b975f838f3621508ba6632f12aa8b2a08fd4c
mwlk <mirkoivowlk@gmail.com>
2025-05-21 01:56:42 -0300

    feat: add use case for suppliers

---
+++ CoreMine.ApplicationBusiness/UseCases/Suppliers/Queries/GetSuppliersQuery.cs
@@ -0,0 +1,12 @@
+﻿using CoreMine.ApplicationBusiness.Common.Queries;
+
+namespace CoreMine.ApplicationBusiness.UseCases.Suppliers.Queries
+{
+    public class GetSuppliersQuery : PaginatedQuery
+    {
+        public int? Id { get; set; }
+        public int[]? Ids { get; set; }
+        public string? FullName { get; set; }
+
+    }
+}

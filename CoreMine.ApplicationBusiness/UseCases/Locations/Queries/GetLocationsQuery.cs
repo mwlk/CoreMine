7bbf2fb2acdd623dc6a7f02f9ba461fe2e22dd27
mwlk <mirkoivowlk@gmail.com>
2025-05-20 22:47:13 -0300

    feat: add repository and handler for locations

---
+++ CoreMine.ApplicationBusiness/UseCases/Locations/Queries/GetLocationsQuery.cs
@@ -0,0 +1,12 @@
+﻿using CoreMine.ApplicationBusiness.Common.Queries;
+
+namespace CoreMine.ApplicationBusiness.UseCases.Locations.Queries
+{
+    public class GetLocationsQuery : PaginatedQuery
+    {
+        public int? Id { get; set; }
+        public int[]? Ids { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}

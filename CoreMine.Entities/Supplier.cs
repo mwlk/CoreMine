ebcb9d2a36030f8f33d85e29e8d6b84cb757a99b
mwlk <mirkoivowlk@gmail.com>
2025-05-21 01:57:18 -0300

    refactor: change nulleable property

--- CoreMine.Entities/Supplier.cs
+++ CoreMine.Entities/Supplier.cs
@@ -5,7 +5,7 @@ namespace CoreMine.Entities
     public class Supplier : NameableEntity
     {
         public string Surname { get; set; } = null!;
-        public string Contact { get; set; } = null!;
+        public string? Contact { get; set; } = null!;
         public string Phone { get; set; } = null!;
         public DateTime CreatedAt { get; set; }
         public virtual ICollection<Product> Products { get; set; }

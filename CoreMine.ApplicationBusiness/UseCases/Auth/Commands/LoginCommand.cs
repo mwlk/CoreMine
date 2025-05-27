72a9ccb83152af104e445f2568e762c9fa660efe
mirko.wlk <mirko.wlk@evoltis.com>
2025-05-27 04:16:00 -0300

    feat: add use case for login

---
+++ CoreMine.ApplicationBusiness/UseCases/Auth/Commands/LoginCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreMine.ApplicationBusiness.UseCases.Auth.Commands
+{
+    public class LoginCommand
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ CoreMine.ApplicationBusiness/UseCases/Auth/Handlers/LoginCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using CoreMine.ApplicationBusiness.Interfaces.Shared;
+using CoreMine.ApplicationBusiness.Services;
+using CoreMine.ApplicationBusiness.UseCases.Auth.Commands;
+
+namespace CoreMine.ApplicationBusiness.UseCases.Auth.Handlers
+{
+    public class LoginCommandHandler : ICommandHandler<LoginCommand, LoginResponse>
+    {
+        private readonly TokenService _tokenService;
+
+        public LoginCommandHandler(TokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+
+        public Task<LoginResponse> HandleAsync(LoginCommand command, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (command.Username == "admin" && command.Password == "password")
+            {
+                var token = _tokenService.GenerateToken(command.Username);
+
+                return Task.FromResult(new LoginResponse
+                {
+                    Success = true,
+                    Token = token,
+                    Username = command.Username,
+                });
+            }
+
+            return Task.FromResult(new LoginResponse
+            {
+                Success = false,
+                Error = "Credenciales ingresadas no son correctas"
+            });
+        }
+    }
+}

f356661306edac1fe8f3d9c2f2015dc863020d4a
mirko.wlk <mirko.wlk@evoltis.com>
2025-05-27 04:17:07 -0300

    feat: update login and product's list

--- CoreMine.Client/Program.cs
+++ CoreMine.Client/Program.cs
@@ -14,9 +14,10 @@ builder.Services.AddRadzenComponents();
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
-builder.Services.AddScoped<AuthService>();
 
+builder.Services.AddScoped<CustomAuthStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(sp => sp.GetRequiredService<CustomAuthStateProvider>());
+builder.Services.AddScoped<AuthService>();
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");

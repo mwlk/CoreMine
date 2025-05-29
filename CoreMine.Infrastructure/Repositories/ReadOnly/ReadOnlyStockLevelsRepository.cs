fa342c86e9d50cdfa80a12ef906466f45bc8a58c
mirko.wlk <mirko.wlk@evoltis.com>
2025-05-29 20:03:55 -0300

    feat: add read only repository

---
+++ CoreMine.Infrastructure/Repositories/ReadOnly/ReadOnlyStockLevelsRepository.cs
@@ -0,0 +1,22 @@
+﻿using CoreMine.ApplicationBusiness.Interfaces.ReadOnly;
+using CoreMine.Data;
+using CoreMine.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreMine.Infraestructure.Repositories.ReadOnly
+{
+    public class ReadOnlyStockLevelsRepository : IReadOnlyStockLevelsRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ReadOnlyStockLevelsRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<StockLevel> GetQueryable()
+        {
+            return _context.StockLevels.AsNoTracking();
+        }
+    }
+}
---
+++ CoreMine.Models/ViewModels/StockLevelViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CoreMine.Models.ViewModels
+{
+    public class StockLevelViewModel
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public int LocationId { get; set; }
+        public string LocationName { get; set; } = string.Empty;
+        public decimal MaxQuantity { get; set; }
+        public decimal MinQuantity { get; set; }
+        public DateTime DefinedAt { get; set; }
+    }
+}

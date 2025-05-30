df95269bd65d3a82906fdd39487491241355a78d
mirko.wlk <mirko.wlk@evoltis.com>
2025-05-29 23:51:32 -0300

    feat: add invoices

---
+++ CoreMine.Entities/PurchaseInvoice.cs
@@ -0,0 +1,17 @@
+﻿namespace CoreMine.Entities
+{
+    public class PurchaseInvoice
+    {
+        public int Id { get; set; }
+        public string InvoiceNumber { get; set; } = string.Empty;
+        public DateTime IngresedAt { get; set; }
+        public int SupplierId { get; set; }
+        public Supplier Supplier { get; set; } = null!;
+        public virtual ICollection<PurchaseInvoiceDetail> PurchaseInvoiceDetails { get; set; }
+
+        public PurchaseInvoice()
+        {
+            PurchaseInvoiceDetails = new HashSet<PurchaseInvoiceDetail>();
+        }
+    }
+}

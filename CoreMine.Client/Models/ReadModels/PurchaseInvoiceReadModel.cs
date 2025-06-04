59ad442855ea5c21c6508223103373871430559b
mirko.wlk <mirko.wlk@evoltis.com>
2025-06-04 01:55:02 -0300

    feat: update option names

---
+++ CoreMine.Client/Models/ReadModels/PurchaseInvoiceReadModel.cs
@@ -0,0 +1,26 @@
+﻿namespace CoreMine.Client.Models.ReadModels
+{
+    public class PurchaseInvoiceReadModel
+    {
+        public int Id { get; set; }
+        public int SupplierId { get; set; }
+        public string SupplierName { get; set; } = string.Empty;
+        public DateTime IngresedAt { get; set; }
+        public decimal Total { get; set; }
+        public List<PurchaseInvoiceDetailReadModel> Details { get; set; }
+
+        public PurchaseInvoiceReadModel()
+        {
+            Details = new List<PurchaseInvoiceDetailReadModel>();
+        }
+    }
+
+    public class PurchaseInvoiceDetailReadModel
+    {
+        public int ProductId { get; set; }
+        public string ProductCode { get; set; } = string.Empty;
+        public string ProductName { get; set; } = string.Empty;
+        public decimal Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
